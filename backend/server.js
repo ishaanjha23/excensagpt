ea667c4b9410e83e962366a2fcb64bfd3ed3def8
Ishaan Jha <ishaanjha@Ishaans-MacBook-Air.local>
2025-07-05 16:46:40 +0530

    🧼 Fix duplicate path import

--- backend/server.js
+++ backend/server.js
@@ -218,8 +218,6 @@ except Exception as e:
     });
   });
 }
-import path from 'path';
-import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
